7442fb381a52d6ee69c3e80bbe7ec5169f0d874d
jb63 <jb63@sanger.ac.uk>
2025-10-02 11:35:15 +0000

    Add md process

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,6 +26,11 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
